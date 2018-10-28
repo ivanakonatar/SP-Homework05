52d8fc93381d65fd36c4e1a708ed3f2a35e44211
ivanakonatar <Nanica2311>
2018-10-28 17:17:49 +0100

    Homework05

---
+++ task4.py
@@ -0,0 +1,35 @@
+""""
+Write a Python program that inputs a document and then outputs a bar- chart plot
+ of the frequencies of each alphabet character that appears in that document.
+"""
+
+import matplotlib.pyplot as plt
+
+
+x = input("Your document: ")
+
+
+d = {}
+length = len(x)
+
+y = 0
+
+for i in range(length):
+
+    if x[i] not in d and x[i] != ' ':
+
+        for j in range(i, length):
+
+            if x[i] == x[j]:
+                y += 1
+
+        d[x[i]] = y
+        y = 0
+
+
+
+
+plt.bar(range(len(d)), list(d.values()), align = 'center')
+plt.xticks(range(len(d)), list(d.keys()))
+
+plt.show()
---
+++ task7.py
@@ -0,0 +1,28 @@
+"""
+Implement a recursive function with signature `find(path, filename)` that reports all entries of the file system rooted
+at the given path having the given file name. """
+
+import os
+
+
+def find(path, filename):
+
+    file = os.listdir(path)
+
+    for i in file:
+
+        if os.path.isdir(path + '\\' + i):
+
+            find(path + '\\' + i, filename)
+
+        else:
+
+            if i == filename:
+                print(os.path.basename(path))
+
+
+find("C:\\Users\\Ivana\\PycharmProjects", "task1.py")
+
+
+
+
